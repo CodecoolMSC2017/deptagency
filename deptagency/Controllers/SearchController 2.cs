ca9ca034ee107d4252d9172be486dd9f21406d19
norcs <norbertcs@me.com>
2018-11-28 12:32:39 +0100

    Update settings

---
+++ deptagency/Controllers/SearchController 2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using deptagency.Model;
+using deptagency.Services;
+using deptagency.Services.ServiceImpl;
+using Microsoft.AspNetCore.Mvc;
+
+namespace deptagency.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        readonly IProduceContentService _produceContent;
+
+        public SearchController(IProduceContentService produceContentService)
+        {
+            _produceContent = produceContentService;
+        }
+
+        [HttpGet]
+        [Route("{term}")]
+        public ActionResult<List<ResultDto>> GetResult(string term)
+        {
+            return _produceContent.CreateContent(term).Result;
+        }
+    }
+}
---
+++ deptagency/Services/ServiceImpl/ProduceContentService 2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using deptagency.Model;
+
+namespace deptagency.Services.ServiceImpl
+{
+    public class ProduceContentService : IProduceContentService
+    {
+        readonly IMovieSearchService _movieSearch;
+        readonly ITrailerSearchService _trailerSearch;
+
+        public ProduceContentService(IMovieSearchService movieSearch, ITrailerSearchService trailerSearch)
+        {
+            _movieSearch = movieSearch;
+            _trailerSearch = trailerSearch;
+        }
+
+        public async Task<List<ResultDto>> CreateContent(string term)
+        {
+            List<ResultDto> results = new List<ResultDto>();
+            List<Movie> movies = _movieSearch.FindMovie(term).Result;
+
+
+            foreach (var movie in movies)
+            {
+
+                ResultDto resultDto = new ResultDto();
+                resultDto.Title = movie.Title;
+                resultDto.ImdbRating = movie.imdbID;
+
+                resultDto.TrailerList = await Task.WhenAll(_trailerSearch.FindTrailer(movie.Title));
+
+                results.Add(resultDto);
+            }
+            return results;
+        }
+    }
+}
